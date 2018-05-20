305ab5e23c78ce641104c40b5caf0021350b7ffa
Yujin Cho <yujinjcho@gmail.com>
2018-05-20 08:52:40 -0700

    add ch2 q up to exercise 18

---
+++ src/main/scala/problems/chapter3/List.scala
@@ -0,0 +1,108 @@
+package problems.chapter3
+
+sealed trait List[+A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+	def sum(ints: List[Int]): Int = ints match {
+	  case Nil => 0
+	  case Cons(x,xs) => x + sum(xs)
+	}
+
+	def product(ds: List[Double]): Double = ds match {
+	  case Nil => 1.0
+	  case Cons(0.0, _) => 0.0
+	  case Cons(x,xs) => x * product(xs)
+	}
+
+	def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+  // Exercise 2 - implement tail function for removing first element
+  def tail[A](ds: List[A]): List[A] = ds match {
+      case Nil => Nil
+      case Cons(_, xs) => xs
+    }
+
+  // Exercise 3 - generalize tail to drop n elements
+  def drop[A](ds: List[A], n: Int): List[A] =
+    if (n == 0) ds
+    else ds match {
+      case Nil => Nil
+      case Cons(_, xs) => drop(xs, n-1)
+    }
+
+  // Exercise 4 - Implement dropWhile
+  def dropWhile[A](l: List[A])(f: A => Boolean): List[A] =
+    l match {
+      case Cons(x, xs) if f(x) => dropWhile(xs)(f)
+      case _ => l
+    }
+
+  // Exercise 5 - Implement setHead
+  def setHead[A](ds: List[A], a: A): List[A] = {
+    ds match {
+      case Nil => Cons(a, Nil)
+      case Cons(_, xs) => Cons(a, xs)
+    }
+  }
+
+  // Exercise 6 - implement init which drops last item
+  def init[A](l: List[A]): List[A] = l match {
+    case Cons(_, Nil) => Nil
+    case Cons(x, xs) => Cons(x, init(xs))
+  }
+
+  // Exercise 9 - compute lenght using foldRight
+  def length[A](l: List[A]): Int =
+    foldRight(l, 0)((_, acc) => acc + 1)
+
+
+  def append[A](a1: List[A], a2: List[A]): List[A] =
+    a1 match {
+      case Nil => a2
+      case Cons(h,t) => Cons(h, append(t, a2))
+    }
+
+  def foldRight[A,B](l: List[A], z: B)(f: (A, B) => B): B = 
+    l match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  // Exercise 10: do a tail recursive foldLeft
+  @annotation.tailrec
+  def foldLeft[A,B](l: List[A], z: B)(f: (A, B) => B): B = 
+    l match {
+      case Nil => z
+      case Cons(x, xs) => foldLeft(xs, f(x,z))(f)
+    }
+
+  // Exercise 11: sum, product, length with foldLeft
+  def sumFoldLeft(l: List[Int]): Int = foldLeft(l, 0)(_ + _)
+  def productFoldLeft(l: List[Double]): Double = foldLeft(l, 1.0)(_ * _)
+  def lengthFoldLeft(l: List[Int]): Int = foldLeft(l, 0)((x, acc) => acc + 1)
+
+  // Exercise 12: reverse with fold
+  def reverse[A](l: List[A]): List[A] =
+     foldLeft(l, Nil: List[A])(Cons(_,_))
+
+  // Exercise 13: implement foldLeft with foldRight
+   def foldLeftViaFoldRight[A,B](l: List[A], z: B)(f: (A, B) => B): B = 
+     foldRight(l, (b:B) => b)((a, g) => b => g(f(a, b)))(z)
+
+  def foldRightViaFoldLeft[A,B](l: List[A], z: B)(f: (A, B) => B): B = 
+     foldLeft(l, (b:B) => b)((a, g) => b => g(f(a, b)))(z)
+
+
+  // Exercise 14: implement append via fold
+  def appendViaFoldRight[A](l: List[A], r: List[A]): List[A] = 
+    foldRight(l, r)(Cons(_,_))
+
+  // Exercise 15: implement function to concat list of list into single list
+  def flatMap[A](ll: List[List[A]]): List[A] = 
+    foldRight(ll, Nil: List[A])(append)
+
+}
---
+++ src/test/scala/Chapter3Spec.scala
@@ -0,0 +1,102 @@
+package problems.chapter3
+
+import org.scalatest._
+
+class Chapter3Spec extends FeatureSpec {
+  feature("functional data structures and data sharing") {
+    scenario("2 - implement tail function for removing first element") {
+      val a = List(1,2,3)
+      assert(List.tail(a) == List(2,3))
+    }
+
+    scenario("3 - generalize tail to drop n elements") {
+      val a = List(1,2,3)
+      assert(List.drop(a, 2) == List(3))
+    }
+
+    scenario("4 - implement dropWhile") {
+      val a = List(1,2,3,4,5)
+      assert(List.dropWhile(a)(_ < 3) == List(3,4,5))
+    }
+
+    scenario("5 - implement setHead") {
+      val a = List(1,2)
+      assert(List.setHead(a, 0) == List(0,2))
+    }
+
+    scenario("6 - implement init which drops last item") {
+      val a = List(1,2,3)
+      assert(List.init(a) == List(1,2))
+    }
+
+    scenario("9 - implement length using foldRight") {
+      val a = List(1,2,3)
+      assert(List.length(a) == 3)
+    }
+  }
+
+  feature("recursion and generalizing to higher order functions") {
+    scenario("10 - compute length of list using foldRight") {
+      val l = List(1,2,3)
+      val length = List.foldRight(l, 0)((_, acc) => acc + 1)
+      assert(length == 3)
+    }
+
+    scenario("11 - write sum, product, length using foldLeft") {
+      val l = List(4,4,4)
+      assert(List.sumFoldLeft(l) == 12)
+      assert(List.lengthFoldLeft(l) == 3)
+
+      val l2 = List(4.0, 4.0, 4.0)
+      assert(List.productFoldLeft(l2) == 64)
+    }
+
+    scenario("12 - write reverse using fold") {
+      val l = List(1,2,3)
+      assert(List.reverse(l) == List(3,2,1))
+    }
+
+    scenario("13 - write foldLeft in terms of foldRight and vice versa") {
+      val l = List(1,2,3)
+      val reversedL = List.foldLeftViaFoldRight(l, Nil:List[Int])(Cons(_,_))
+      val notReversed = List.foldRightViaFoldLeft(l, Nil:List[Int])(Cons(_,_))
+      assert(reversedL == List(3,2,1))
+      assert(notReversed == l)
+    }
+
+    scenario("14 - implement append via fold") {
+      val l = List.appendViaFoldRight(List(1,2), List(3,4))
+      assert(l == List(1,2,3,4))
+    }
+
+    scenario("15 - implement func to concat list of list into single list") {
+      val l = List.flatMap(List(List(1,2), List(3,4), List(5)))
+      assert(l == List(1,2,3,4,5))
+    }
+  }
+
+  feature("more functions for working with lists") {
+    scenario("16 - add 1 to each List[Int]") {
+      def addOne(l: List[Int]): List[Int] =
+        List.foldRight(l, Nil:List[Int])((x, acc) => Cons(x + 1,acc))
+      val l = List(1,2,3)
+      assert(addOne(l) == List(2,3,4))
+    }
+
+    scenario("17 - List[Double] to List[String]") {
+      def doubleToString(l: List[Double]): List[String] =
+        List.foldRight(l, Nil:List[String])((x, acc) => Cons(x.toString,acc))
+      val l = List(1.0,2.0)
+      assert(doubleToString(l) == List("1.0", "2.0"))
+    }
+
+    scenario("18 - implement map") {
+      def map[A,B](l: List[A])(f: A => B): List[B] = 
+        List.foldRight(l, Nil: List[B])((x, acc) => Cons(f(x), acc))
+      val l = List(1.0,2.0)
+      val lStringified = map(l)((a) => a.toString)
+      assert(lStringified == List("1.0", "2.0"))
+    }
+
+  }
+}
