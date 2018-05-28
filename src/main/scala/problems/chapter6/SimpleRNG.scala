747365ad8f9ea858617a256a01f23086060db44e
Yujin Cho <yujinjcho@gmail.com>
2018-05-28 16:56:36 -0700

    exercise 6.2

---
+++ src/main/scala/problems/chapter6/SimpleRNG.scala
@@ -0,0 +1,19 @@
+package problems.chapter6
+
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+case class SimpleRNG(seed: Long) extends RNG {
+  def nextInt: (Int, RNG) = {
+    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+    val nextRNG = SimpleRNG(newSeed)
+    val n = (newSeed >>> 16).toInt
+    (n, nextRNG)
+  }
+
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (n, nextRng) = rng.nextInt
+    (if (n < 0) -(n + 1) else n, nextRng)
+  }
+}
