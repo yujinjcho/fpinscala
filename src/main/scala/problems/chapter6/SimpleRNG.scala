afd6d19fe1aab51dda580afd541619ae3ede41ea
Yujin Cho <yujinjcho@gmail.com>
2018-05-29 21:12:42 -0700

    exercise 6.8

--- src/main/scala/problems/chapter6/SimpleRNG.scala
+++ src/main/scala/problems/chapter6/SimpleRNG.scala
@@ -93,4 +93,10 @@ case class SimpleRNG(seed: Long) extends RNG {
   def intsViaSequence(count: Int)(rng: RNG): (List[Int], RNG) = {
     sequence(List.fill(count)(int))
   }
+
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng => {
+      val (a, r1) = f(rng)
+      g(a)(r1)
+    }
 }
