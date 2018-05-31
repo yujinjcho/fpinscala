4753aabd49ae879369e30f019b231698509c8379
Yujin Cho <yujinjcho@gmail.com>
2018-05-30 22:22:40 -0700

    up to exercise 6.10

--- src/main/scala/problems/chapter6/SimpleRNG.scala
+++ src/main/scala/problems/chapter6/SimpleRNG.scala
@@ -1,12 +1,12 @@
 package problems.chapter6
 
-
 trait RNG {
   def nextInt: (Int, RNG)
 }
 
 case class SimpleRNG(seed: Long) extends RNG {
-  type Rand[+A] = RNG => (A, RNG)
+  type State[S,+A] = S => (A,S)
+  type Rand[A] = State[RNG, A]
 
   def nextInt: (Int, RNG) = {
     val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
@@ -58,10 +58,10 @@ case class SimpleRNG(seed: Long) extends RNG {
 
   def unit[A](a: A): Rand[A] = rng => (a, rng)
 
-  def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+  def map[S,A,B](a: S => (A,S))(f: A => B): S => (B,S) =
     rng => {
-      val (a, rng2) = s(rng)
-      (f(a), rng2)
+      val (x, rng2) = a(rng)
+      (f(x), rng2)
     }
 
   def nonNegativeEven: Rand[Int] =
@@ -90,7 +90,7 @@ case class SimpleRNG(seed: Long) extends RNG {
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
     fs.foldRight(unit(List[A]()))((f,acc) => map2(f, acc)(_ :: _))
 
-  def intsViaSequence(count: Int)(rng: RNG): (List[Int], RNG) = {
+  def intsViaSequence(count: Int): Rand[List[Int]] = {
     sequence(List.fill(count)(int))
   }
 
@@ -99,4 +99,39 @@ case class SimpleRNG(seed: Long) extends RNG {
       val (a, r1) = f(rng)
       g(a)(r1)
     }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] =
+    flatMap(nonNegativeInt) { i =>
+      val mod = i % n
+      if (i + (n-1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
+    }
+
+  def mapViaFlatMap[A,B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s) { i =>
+      unit(f(i))
+    }
+
+  def map2ViaFlatMap[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)(a => map(rb)(b => f(a, b)))
+}
+
+import State._
+
+case class State[S,+A](run: S => (A,S)) {
+  def flatMap[B](f: A => State[S,B]): State[S,B] =
+   State(s => {
+     val (a, s1) = run(s)
+     f(a).run(s1)
+   })
+
+  def map[B](f: A => B): State[S,B] =
+    flatMap(a => unit(f(a)))
+
+  def map2[B,C](sb: State[S, B])(f: (A,B) => C): State[S, C] =
+    flatMap(a => sb.map(b => f(a,b)))
+
+}
+
+object State {
+  def unit[S, A](a: A): State[S,A] = State(s => (a, s))
 }
