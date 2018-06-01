8ee5cf27b8ed269c90d048ca06f4bc9e693e6041
Yujin Cho <yujinjcho@gmail.com>
2018-05-31 23:14:54 -0700

    exercise 6.11 try to break out

--- src/main/scala/problems/chapter6/SimpleRNG.scala
+++ src/main/scala/problems/chapter6/SimpleRNG.scala
@@ -129,9 +129,52 @@ case class State[S,+A](run: S => (A,S)) {
 
   def map2[B,C](sb: State[S, B])(f: (A,B) => C): State[S, C] =
     flatMap(a => sb.map(b => f(a,b)))
-
 }
 
 object State {
   def unit[S, A](a: A): State[S,A] = State(s => (a, s))
+
+  def modify[S](f: S => S): State[S, Unit] = for {
+    s <- get
+    _ <- set(f(s))
+  } yield ()
+
+  def get[S]: State[S, S] = State(s => (s, s))
+
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+
+  def sequence[S,A](sas: List[State[S, A]]): State[S, List[A]] =
+    sas.foldRight(unit[S, List[A]](List()))((f, acc) => f.map2(acc)(_ :: _))
+}
+
+
+sealed trait Input
+case object Coin extends Input
+case object Turn extends Input
+case class Machine(locked: Boolean, candies: Int, coins: Int)
+
+
+object CandyMachine {
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] =
+    sequence(
+      inputs.map(i => {
+        val m: (Machine => Machine) => State[Machine, Unit] = modify[Machine] _
+        val f: Input => State[Machine, Unit] = (m compose update)
+        val r: State[Machine, Unit] = f(i)
+        r
+      })
+    ).flatMap( _ =>
+        get.map(s =>
+          (s.coins, s.candies)))
+
+  def update = (i: Input) => (s: Machine) =>
+    (i, s) match {
+      case (_, Machine(_, 0, _)) => s
+      case (Coin, Machine(false, _, _)) => s
+      case (Turn, Machine(true, _, _)) => s
+      case (Coin, Machine(true, candy, coin)) =>
+        Machine(false, candy, coin + 1)
+      case (Turn, Machine(false, candy, coin)) =>
+        Machine(true, candy - 1, coin)
+    }
 }
