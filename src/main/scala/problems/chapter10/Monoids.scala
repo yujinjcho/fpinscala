3a8e511d96765f49d9188a28c9f37e77b50eb6e8
Yujin Cho <yujinjcho@gmail.com>
2018-06-10 12:40:57 -0700

    10.2

--- src/main/scala/problems/chapter10/Monoids.scala
+++ src/main/scala/problems/chapter10/Monoids.scala
@@ -11,6 +11,12 @@ object Chapter10 {
     val zero = ""
   }
 
+  def listMonoid[A] = new Monoid[List[A]] {
+    def op(a1: List[A], a2: List[A]) = a1 ++ a2
+    val zero = Nil
+  }
+
+  // Exercise 10.1
   val intAddition = new Monoid[Int] {
     def op(a1: Int, a2: Int) = a1 + a2
     val zero = 0
@@ -22,12 +28,17 @@ object Chapter10 {
   }
 
   val booleanOr = new Monoid[Boolean] {
-    def op(a1: Boolean, a2: Boolean) = a1 | a2
+    def op(a1: Boolean, a2: Boolean) = a1 || a2
     val zero = false
   }
 
   val booleanAnd = new Monoid[Boolean] {
-    def op(a1: Boolean, a2: Boolean) = a1 & a2
+    def op(a1: Boolean, a2: Boolean) = a1 && a2
     val zero = true
+  }
+
+  def optionMonoid[A]: new Monoid[Option[A]] {
+    def op(a1: Option[A], a2: Option[A]) =
+    val zero = None
   }
 }
